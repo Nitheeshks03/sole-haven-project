278d636ffc341b7efe6badedfc02d14dbba3c819
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-23 00:14:40 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/Frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "Frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"))
   );
 } else {
   const __dirname = path.resolve();
