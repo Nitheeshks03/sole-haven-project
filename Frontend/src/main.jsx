a50fa93c6f13f1aab2823fede6f05688c0ecb5ee
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-22 22:40:35 +0530

    final

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import { Notifications } from '@mantine/notifications';
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <QueryClientProvider client={queryClient}>
         <Notifications position='top-right'/>
         <App />
-        <ReactQueryDevtools  />
         </QueryClientProvider>
       </MantineProvider>
     </BrowserRouter>
