5e50cd3399ab698e5b5fed51d9460f6b151322b4
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-22 23:51:35 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/Frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "Frontend", "build", "index.html"))
