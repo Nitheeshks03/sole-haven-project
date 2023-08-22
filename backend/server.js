2b0a0e5ba5c679000c63e06962826df026082bda
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-22 23:59:47 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -26,17 +26,17 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.use(express.static(path.join(__dirname, "/Frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "Frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
