6735941005b32aaf71c4f58f31e310b1370e0974
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-22 19:19:01 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+
+
 
 app.use(cookieParser());
 
@@ -35,6 +32,19 @@ app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
