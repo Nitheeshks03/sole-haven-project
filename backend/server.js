55cb23fd42aa85812c52f7c45202a9a1279e1da9
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-22 19:48:14 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
@@ -29,21 +27,20 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
 }
-
-
 
 app.use(errorHandler);
 
