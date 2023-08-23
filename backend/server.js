08bd3732a71f036b462b427ffcaf419f5ed21e8d
Nitheeshks03 <127733144+Nitheeshks03@users.noreply.github.com>
2023-08-23 11:24:28 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use("/api/upload", uploadRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(
-    "/uploads",
-    express.static(path.join(__dirname, "../Frontend/public/images"))
+    "/upload",
+    express.static(path.join(__dirname, "/Frontend/dist/images"))
   );
   app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
